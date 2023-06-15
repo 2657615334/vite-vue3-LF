5efc435098c273e86de669fa127ddb9ebc4515c7
Yi Handong <2657615334@qq.com>
2023-06-15 11:08:01 +0800

    fix(配置): 修改打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -35,10 +35,12 @@ export default defineConfig({
     rollupOptions: {
       // vite打包是通过rollup来打包的
       output: {
-        manualChunks: (id: string) => {
-          // 可以在这里打印看一下id的值，这里做个简单处理将node_modules中的包打包为vendor文件
-          if (id.indexOf('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+        chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
+        entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
+        assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
           }
         },
       },
